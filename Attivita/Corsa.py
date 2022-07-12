f2f841ae399dc37d7549f2ee723d6df8106ba1c1
Matteo <s1098185@studenti.univpm.it>
2022-07-12 13:06:10 +0200

    attivita

--- Attivita/Corsa.py
+++ Attivita/Corsa.py
@@ -1,6 +1,5 @@
 import datetime
-import os
-import pickle
+import uuid
 
 from Attivita.Ricevuta import Ricevuta
 
@@ -9,43 +8,23 @@ class Corsa:
 
     def __init__(self):
         self.ricevuta = None
-        self.cliente = None
+        self.mezzo = None
         self.codice = ""
-        self.dataInizio = datetime.datetime(year=1970, month=1, day=1)
-        self.dataFine = datetime.datetime(year=1970, month=1, day=1)
+        self.data_inizio = datetime.datetime(year=1970, month=1, day=1)
+        self.data_fine = datetime.datetime(year=1970, month=1, day=1)
 
-    # inizializza una nuova corsa
-    def iniziaCorsa(self, cliente, monopattino):
-        self.codice = #todo codice
-        self.dataInizio = datetime.datetime.now().replace(microsecond=0)
-        self.cliente = cliente
-        self.monopattino = monopattino
-        self.ricevuta = Ricevuta()
+    def inizializza_corsa(self, mezzo):
+        self.mezzo = mezzo
+        self.codice = uuid.uuid4()[:8]
 
-        self.codiceMezzo = self.monopattino.getCodiceMezzoDisponibile()
-        self.monopattino.setDisponibilita(self.codiceMezzo, False)
+    # avvia una nuova corsa
+    def avvia_corsa(self):
+        self.data_inizio = datetime.datetime.now().replace(microsecond=0)
 
     # termina una corsa
-    def terminaCorsa(self):
+    def termina_corsa(self, cliente):
         self.dataFine = datetime.datetime.now().replace(microsecond=0)
-        self.ricevuta.stilaRicevuta(self.dataInizio, self.dataFine, self.cliente.codice)
-        self.monopattino.setDisponibilita(self.codiceMezzo, True)
-        self.cliente.portafoglio.prelevaDenaro(self.ricevuta.costoTotale)
-
-        corse = {}
-        if os.path.isfile("Dati/Corse.pickle"):
-            with open("Dati/Corse.pickle", "rb") as f:
-                corse = pickle.load(f)
-
-        corse[self.codice] = self
-        with open("Dati/Corse.pickle", "wb") as f:
-            pickle.dump(corse, f, pickle.HIGHEST_PROTOCOL)
-
-    # ritorna le corse di tutti i clienti
-    def getCorse(self):
-        if os.path.isfile("Dati/Corse.pickle"):
-            with open("Dati/Corse.pickle", "rb") as f:
-                corse = dict(pickle.load(f))
-                return corse
-        else:
-            return None
+        self.ricevuta = Ricevuta()
+        self.ricevuta.stilaRicevuta(codice_mezzo=self.mezzo.codice, codice_corsa=self.codice,
+                                    codice_cliente=cliente.codice, inizio=self.data_inizio, fine=self.data_fine)
+        del self
